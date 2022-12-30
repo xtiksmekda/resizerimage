1de0a1c16f04f99cfeb0c3b944d45a0af2936ca8
Izumi Sagiri <74088875+aldy57@users.noreply.github.com>
2022-12-30 10:01:26 +0700

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    let date = new Date()
+    let clock = document.getElementById('clock')
+    clock.innerHTML = 
+      date.getHours()+":"+
+      date.getMinutes()+":"+
+      date.getSeconds()
+  }, 1000);
